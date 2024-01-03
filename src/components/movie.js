710895ac286d7ee55a27a59006539525c5453289
LukeDude200 <croslynlc@gmail.com>
2024-01-03 16:31:39 -0400

    Individual Movie Page

--- src/components/movie.js
+++ src/components/movie.js
@@ -1,7 +1,87 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import MovieDataService from "../services/movies";
+import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
-function Movie() {
-	return <div className="App">Movie</div>;
-}
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+// import Button from "react-bootstrap/Button";
+
+const Movie = ({ user }) => {
+	// useParams allows us to get the ID directly from the URL
+	const { id } = useParams();
+
+	// Set some variables for what we want to show
+	const [movie, setMovie] = useState({
+		id: null,
+		title: "",
+		rated: "",
+		reviews: []
+	});
+
+	const [imageError, setImageError] = useState(false);
+
+	const handleImageError = (error) => {
+		console.error("Error loading image: ", error);
+		setImageError(true);
+	};
+
+	const getMovie = (id) => {
+		// Calls the movie service to update the movie information we are displaying
+		MovieDataService.get(id)
+			.then((response) => {
+				setMovie(response.data);
+				console.log(response.data);
+			})
+			.catch((e) => {
+				console.log(e);
+			});
+	};
+
+	useEffect(() => {
+		// Use the id extracted from the URL from using useParams earlier
+		if (id) {
+			getMovie(id);
+		}
+	}, [id]);
+
+	return (
+		<div>
+			<Container>
+				<Row>
+					<Col>
+						{movie.poster && !imageError ? (
+							<Image
+								src={movie.poster}
+								fluid
+								onError={(e) => handleImageError(e)}
+							/>
+						) : (
+							<p>NO POSTER IMAGE AVAILABLE</p>
+						)}
+					</Col>
+					<Col>
+						<Card>
+							<Card.Header as="h5">{movie.title}</Card.Header>
+							<Card.Body>
+								<Card.Text>{movie.plot}</Card.Text>
+								{user && (
+									<Link to={`/movies/${id}/review`}>
+										Add Review
+									</Link>
+								)}
+							</Card.Body>
+						</Card>
+						<br></br>
+						<h2>Reviews</h2>
+					</Col>
+				</Row>
+			</Container>
+		</div>
+	);
+};
 
 export default Movie;
